532f4f406dbc4f16139d22c9e148ec2336613716
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-02-24 20:23:00 -0400

    Friends done

--- app/routes/friends/friend.js
+++ app/routes/friends/friend.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params){
+    return this.store.findRecord('friend', params.id);
+  },
+
+  actions: {
+    delete(friend){
+      console.log('deleting', friend);
+      friend.deleteRecord();
+      friend.save();
+      this.transitionTo('friends');
+    }
+  }
 });
--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -1,4 +1,44 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller, model){
+    this._super(controller, model);
+    controller.set('toppings', this.store.findAll('topping'));
+    controller.set('likes', Ember.A());
+  },
+
+  actions: {
+    create(firstName, lastName, likes){
+      if (!firstName || !lastName || !likes){
+        console.log('missing input');
+        return;
+      }
+
+      let friend = this.store.createRecord('friend', {
+        firstName: firstName,
+        lastName: lastName,
+        likes: likes
+      });
+
+      console.log('friend', friend);
+
+      friend.save().then(
+        (record)=>{
+          console.log('new friend saved', record);
+          this.send('resetForm');
+        },
+        (error)=>{
+          console.log('error saving friend', error);
+        }
+      );
+    },
+
+    resetForm(){
+      this.controller.setProperties({
+        firstName: null,
+        lastName: null,
+        likes: Ember.A()
+      });
+    }
+  }
 });
